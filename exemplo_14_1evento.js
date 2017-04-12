1d4b1614fcd98166034056c4c7089f9ffcb26cd6
isaias54 <rodriguesisaias774@gmail.com>
2017-04-12 15:21:33 -0300

    Add files via upload

---
+++ exemplo_14_1evento.js
@@ -0,0 +1,13 @@
+
+           function evento_entrar()
+             {
+               alert("seja bem vindo");
+             }
+           function evento_sair()
+             {
+               alert("até a próxima visita");
+             }
+           function evento_clique()
+             {
+               window.document.getElementById("id2").style.color="red";
+             }
